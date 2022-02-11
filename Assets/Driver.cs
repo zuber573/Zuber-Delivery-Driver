94d4e9b53944e50207e4b04f43e33a259ec4da7b
Brian Zuber <zuber573@morrisville.edu>
2022-02-11 17:32:05 -0500

    Using Input.GetAxis()

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -15,7 +15,9 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       transform.Rotate(0, 0, steerSpeed);
-       transform.Translate(0 ,movespeed , 0);
+       float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+       float moveAmount = Input.GetAxis("Vertical") * movespeed;
+       transform.Rotate(0, 0, -steerAmount);
+       transform.Translate(0 ,moveAmount , 0);
     }
 }
