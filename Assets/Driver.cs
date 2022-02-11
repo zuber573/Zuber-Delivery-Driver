bb2ce70a9d73cc035367ccb5e494599ea3779ca4
Brian Zuber <zuber573@morrisville.edu>
2022-02-11 17:49:33 -0500

    Colliders & Rigidbodies

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -15,8 +15,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-       float moveAmount = Input.GetAxis("Vertical") * movespeed;
+       float steerAmount = Input.GetAxis("Horizontal") * steerSpeed *Time.deltaTime;
+       float moveAmount = Input.GetAxis("Vertical") * movespeed * Time.deltaTime;
        transform.Rotate(0, 0, -steerAmount);
        transform.Translate(0 ,moveAmount , 0);
     }
